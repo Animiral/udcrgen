4398278ff23af5a16ce9005c74e79fd7883334b4
Peter Neubauer <peterneubauer2@gmail.com>
2021-05-31 22:22:15 +0200

    Generators: maxleaves, onesided_bent, onesided_straight
    Implemented three new classes of benchmark graphs in the generator
    application.

--- gencases.cpp
+++ gencases.cpp
@@ -28,49 +28,305 @@ const int MAX_PROBLEM_SIZE = 100; //!< size of instances to generate
  *   37   x   x   x   x   x   x   x      < extra
  *      x   x   x   x   x  38   x        < leaves
  */
-void max_branches()
+void max_branches(int size)
 {
-	for (int i = 1; i <= MAX_PROBLEM_SIZE; i++) {
-		EdgeList edges;
+	EdgeList edges;
 
-		for (int j = 0; j < i; j++) {
-			if(j > 0) edges.push_back({ j - 1, j }); // spine
-			edges.push_back({ i + j, j }); // branches top
-			edges.push_back({ 2 * i + j, j }); // branches bottom
-			edges.push_back({ 3 * i + j, i + j }); // leaves top
-			edges.push_back({ 4 * i + j, 2 * i + j }); // leaves bottom
-		}
-
-		const int extras = 5 * i;
-
-		edges.push_back({ extras, 0 }); // start branch 1
-		edges.push_back({ extras + 1, 0 }); // start branch 2
-		edges.push_back({ extras + 2, i - 1 }); // end branch 3
-		edges.push_back({ extras + 3, i - 1 }); // end branch 4
-
-		for (int i = 0; i < 2; i++) {
-			edges.push_back({ extras + 4 + i, extras }); // leaves branch 1
-			edges.push_back({ extras + 6 + i, extras + 1 }); // leaves branch 2
-			edges.push_back({ extras + 8 + i, extras + 2 }); // leaves branch 3
-			edges.push_back({ extras + 10 + i, extras + 3 }); // leaves branch 4
-		}
-
-		edges.push_back({ extras + 12, 2 * i }); // extra leaf on start branch
-		edges.push_back({ extras + 13, 3 * i - 1 }); // extra leaf on end branch
-
-		std::string file = "maxbranches" + std::to_string(i) + ".txt";
-		std::ofstream stream{ file };
-		edges_to_text(stream, edges);
-		stream.close();
+	for (int j = 0; j < size; j++) {
+		if(j > 0) edges.push_back({ j - 1, j }); // spine
+		edges.push_back({ size + j, j }); // branches top
+		edges.push_back({ 2 * size + j, j }); // branches bottom
+		edges.push_back({ 3 * size + j, size + j }); // leaves top
+		edges.push_back({ 4 * size + j, 2 * size + j }); // leaves bottom
 	}
+
+	const int extras = 5 * size;
+
+	edges.push_back({ extras, 0 }); // start branch 1
+	edges.push_back({ extras + 1, 0 }); // start branch 2
+	edges.push_back({ extras + 2, size - 1 }); // end branch 3
+	edges.push_back({ extras + 3, size - 1 }); // end branch 4
+
+	for (int i = 0; i < 2; i++) {
+		edges.push_back({ extras + 4 + i, extras }); // leaves branch 1
+		edges.push_back({ extras + 6 + i, extras + 1 }); // leaves branch 2
+		edges.push_back({ extras + 8 + i, extras + 2 }); // leaves branch 3
+		edges.push_back({ extras + 10 + i, extras + 3 }); // leaves branch 4
+	}
+
+	edges.push_back({ extras + 12, 2 * size }); // extra leaf on start branch
+	edges.push_back({ extras + 13, 3 * size - 1 }); // extra leaf on end branch
+
+	std::string file = "maxbranches" + std::to_string(size) + ".txt";
+	std::ofstream stream{ file };
+	edges_to_text(stream, edges);
+	stream.close();
+}
+
+/**
+ * Generate straight-spine lobsters with a lot of leaves.
+ *
+ * Example with 7 spines: (numbering)
+ *
+ *     3   2                             < leaves
+ *   4   1   0                           < spine 0, branch 1
+ *     5   6                             < leaves
+ *
+ *              10  11                   < leaves
+ *     x   x   9   8  12                 < branch 8
+ *   x   x   x   7                       < spine 7
+ *     x   x
+ *
+ *               x   x
+ *     x   x   x   x   x
+ *   x   x   x   x  13                   < spine 13
+ *     x   x  15  14  18                 < branch 14
+ *              16  17                   < leaves
+ *
+ *               x   x
+ *     x   x   x   x   x
+ *   x   x   x   x   x  19               < spine (no branch)
+ *     x   x   x   x   x
+ *               x   x
+ *
+ *               x   x      23  24
+ *     x   x   x   x   x  22  21  25
+ *   x   x   x   x   x   x  20  26       < two more spines
+ *     x   x   x   x   x  28  27  31
+ *               x   x      29  30
+ *
+ *               x   x       x   x
+ *     x   x   x   x   x   x   x   x  34  35     < leaves
+ *   x   x   x   x   x   x   x   x  32  33  36   < last spine with branch
+ *     x   x   x   x   x   x   x   x  37  38     < leaves
+ *               x   x       x   x
+ */
+void max_leaves(int size)
+{
+	EdgeList edges;
+
+	int n = 0; // number of edges added
+	int prev = 0; // previous spine id
+
+	// start with constant 5-leaf branch
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	// do spine vertices except first and last
+	for (int j = 1; j < size - 1; j++) {
+		int spine = ++n;
+		edges.push_back({ prev, spine });
+		prev = spine;
+
+		if (j % 3 != 0) { // leave space every 3rd spine for more leaves
+			int branch = ++n;
+			edges.push_back({ spine, branch });
+
+			for(int leaf = 0; leaf < 4; leaf++)
+				edges.push_back({ branch, ++n });
+		}
+	}
+
+	// add another 5-leaf branch at the end
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	std::string file = "maxleaves" + std::to_string(size) + ".txt";
+	std::ofstream stream{ file };
+	edges_to_text(stream, edges);
+	stream.close();
+}
+
+/**
+ * Generate lobsters in with every other branch is very heavy.
+ * This leads to forced bends if the branches are placed alternatingly.
+ *
+ * Example with 7 spines: (numbering)
+ *
+ *     3   2                             < leaves
+ *   4   1   0                           < spine 0, branch 1
+ *     5   6                             < leaves
+ *
+ *              10  11                   < leaves
+ *     x   x   9   8  12                 < branch 8
+ *   x   x   x   7                       < spine 7
+ *     x   x
+ *
+ *               x   x
+ *     x   x   x   x   x
+ *   x   x   x   x  13                   < spine 13
+ *     x   x      14                     < branch 14
+ *
+ *               x   x  17  18
+ *     x   x   x   x   x  16  19         < branch 16, leaves
+ *   x   x   x   x   x  15  20           < spine 15
+ *     x   x       x
+ *
+ *               x   x   x   x
+ *     x   x   x   x   x   x   x
+ *   x   x   x   x   x   x   x  25  26
+ *     x   x       x  22  21  23  24  27  < spine 21, 23
+ *                                  28
+ *
+ *               x   x   x   x
+ *     x   x   x   x   x   x   x
+ *   x   x   x   x   x   x   x   x   x
+ *     x   x       x   x   x   x   x   x
+ *                          35  29   x   < spine 29
+ *                        34  30  31     < branch 30, leaves
+ *                          33  32
+ */
+void onesided_bent(int size)
+{
+	EdgeList edges;
+
+	int n = 0; // number of edges added
+	int prev = 0; // previous spine id
+
+	// start with constant 5-leaf branch
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	// do spine vertices except first and last
+	for (int j = 1; j < size - 1; j++) {
+		int spine = ++n;
+		edges.push_back({ prev, spine });
+		prev = spine;
+
+		if (j % 2 > 0) { // heavy side
+			int branch = ++n;
+			edges.push_back({ spine, branch });
+
+			for (int leaf = 0; leaf < 4; leaf++)
+				edges.push_back({ branch, ++n });
+		}
+		else { // light side
+			edges.push_back({ spine, ++n });
+		}
+	}
+
+	// add another 5-leaf branch at the end
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	std::string file = "onesided_bent" + std::to_string(size) + ".txt";
+	std::ofstream stream{ file };
+	edges_to_text(stream, edges);
+	stream.close();
+}
+
+/**
+ * Generate lobsters in with every other branch is very heavy.
+ * With strategic placement, these instances can be embedded on a straight spine.
+ *
+ * Example with 6 spines: (numbering)
+ *
+ *     3   2                             < leaves
+ *   4   1   0                           < spine 0, branch 1
+ *     5   6                             < leaves
+ *
+ *              10  11                   < leaves
+ *     x   x   9   8  12                 < branch 8
+ *   x   x   x   7                       < spine 7
+ *     x   x
+ *
+ *               x   x
+ *     x   x   x   x   x
+ *   x   x   x   x  13  15               < spine 13, 15
+ *     x   x      14                     < branch 14
+ *
+ *               x   x      19  20
+ *     x   x   x   x   x  18  17  21     < branch 17, leaves
+ *   x   x   x   x   x   x  16           < spine 16
+ *     x   x       x
+ *
+ *               x   x       x   x
+ *     x   x   x   x   x   x   x   x
+ *   x   x   x   x   x   x   x  22  24   < spine 22
+ *     x   x       x          28  23  25 < branch 23, leaves
+ *                              27  26
+ */
+void onesided_straight(int size)
+{
+	EdgeList edges;
+
+	int n = 0; // number of edges added
+	int prev = 0; // previous spine id
+
+	// start with constant 5-leaf branch
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	// do spine vertices except first and last
+	for (int j = 1; j < size - 1; j++) {
+		int spine = ++n;
+		edges.push_back({ prev, spine });
+		prev = spine;
+
+		if (j % 3 == 1) { // heavy side
+			int branch = ++n;
+			edges.push_back({ spine, branch });
+
+			for (int leaf = 0; leaf < 4; leaf++)
+				edges.push_back({ branch, ++n });
+		}
+		else if (j % 3 == 1) { // light side
+			edges.push_back({ spine, ++n });
+		}
+	}
+
+	// add another 5-leaf branch at the end
+	{
+		int branch = ++n;
+		edges.push_back({ prev, branch });
+
+		for (int leaf = 0; leaf < 5; leaf++)
+			edges.push_back({ branch, ++n });
+	}
+
+	std::string file = "onesided_straight" + std::to_string(size) + ".txt";
+	std::ofstream stream{ file };
+	edges_to_text(stream, edges);
+	stream.close();
 }
 
 int main(int argc, const char* argv[])
 {
 	std::cout << "Generate input graphs up to size " << MAX_PROBLEM_SIZE << ".\n";
 
-	std::cout << "Run generator max_branches...\n";
-	max_branches();
+	std::cout << "Run generators...\n";
+	for (int i = 1; i <= MAX_PROBLEM_SIZE; i++) {
+		std::cout << "\tsize " << i << "...\n";
+		max_branches(i);
+		max_leaves(i);
+		onesided_bent(i);
+		onesided_straight(i);
+	}
 
-	std::cout << "Generator Done.\n";
+	std::cout << "Generators Done.\n";
 }

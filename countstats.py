e11da8d168eabc7ec002dee5019bebc3d71d8848
Peter Neubauer <peterneubauer2@gmail.com>
2022-10-17 22:03:44 +0200

    Adapted countstats.py to put out more plot-specific aggregate data

--- countstats.py
+++ countstats.py
@@ -5,6 +5,7 @@
 # It requires that stats data rows are grouped by instance ID,
 # and that all algorithms were applied to all instances.
 import csv
+import os
 from collections import namedtuple
 
 # aggregate record type
@@ -20,7 +21,16 @@ Record = namedtuple('Record', [
     'dfsnotime', # cumulative run time of heuristic with dfs on no-instances
     'bfsnotime']) # cumulative run time of heuristic with bfs on no-instances
 
-final = {}
+final = {} # aaggregate data indexed by size, then by number of spines.
+
+# spine-aggregate record type
+SpineRecord = namedtuple('SpineRecord', [
+    'yescnt', # count of yes-instances
+    'dfsyes', # count of instances solved by heuristic with dfs
+    'bfsyes']) # count of instances solved by heuristic with bfs
+
+# Aggregate data by number of spines
+byspine = {}
 
 last_instance="" # current instance being collected from multiple input rows
 dpyes = 0 # whether current instance was solved by reliable algorithm
@@ -43,28 +53,36 @@ def addrecord(size, spines):
 
     if spines not in final[size]:
         final[size][spines] = Record(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        
+    if spines not in byspine:
+        byspine[spines] = SpineRecord(0, 0, 0)
 
     record = final[size][spines]
+    spinerecord = byspine[spines]
     assert dptime > 0
     assert dfstime > 0
     assert bfstime > 0
 
     if dpyes > 0:
         record = record._replace(yescnt=record.yescnt + 1, dpyestime=record.dpyestime + dptime)
+        spinerecord = spinerecord._replace(yescnt=spinerecord.yescnt + 1)
     else:
         record = record._replace(nocnt=record.nocnt + 1, dpnotime=record.dpnotime + dptime)
 
     if dfsyes > 0:
         record = record._replace(dfsyes=record.dfsyes + 1, dfsyestime=record.dfsyestime + dfstime)
+        spinerecord = spinerecord._replace(dfsyes=spinerecord.dfsyes + 1)
     else:
         record = record._replace(dfsnotime=record.dfsnotime + dfstime)
 
     if bfsyes > 0:
         record = record._replace(bfsyes=record.bfsyes + 1, bfsyestime=record.bfsyestime + bfstime)
+        spinerecord = spinerecord._replace(bfsyes=spinerecord.bfsyes + 1)
     else:
         record = record._replace(bfsnotime=record.bfsnotime + bfstime)
                 
     final[size][spines] = record
+    byspine[spines] = spinerecord
     dpyes = 0
     dfsyes = 0
     bfsyes = 0
@@ -113,20 +131,105 @@ with open('stats.csv', 'r') as statsfile:
         addrecord(size, spines)
 
     statsfile.close()
-
-print("Writing...")
+    
+print("Writing Basic Aggregate...")
 
 with open('aggregate.csv', 'w') as aggfile:
     writer = csv.writer(aggfile)
     fields = ['size', 'spines', 'yescnt', 'nocnt', 'dfsyes', 'bfsyes', 'dpyestime', 'dfsyestime', 'bfsyestime', 'dpnotime', 'dfsnotime', 'bfsnotime']
     writer.writerow(fields)
 
-
     for size in final:
         for spines, record in final[size].items():
             fields = [size, spines, record.yescnt, record.nocnt, record.dfsyes, record.bfsyes,
                 record.dpyestime, record.dfsyestime, record.bfsyestime, record.dpnotime, record.dfsnotime, record.bfsnotime]
             writer.writerow(fields)
+
+    aggfile.close()
+    
+print("Writing Runtime(DP) plot data...")
+
+with open('runtime_dp.dat', 'w') as aggfile:
+    fields = ['size', 'dptime', 'clsspines']
+    aggfile.write(" ".join(fields) + os.linesep)
+
+    for size in final:
+        for spines, record in final[size].items():
+            if record.yescnt > 0:
+                fields = [size, record.dpyestime/record.yescnt, "yes" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+            if record.nocnt > 0:
+                fields = [size, record.dpnotime/record.nocnt, "no" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+    aggfile.close()
+
+
+print("Writing Runtime(DP, yes) plot data...")
+
+with open('runtime_dpyes.dat', 'w') as aggfile:
+    fields = ['size', 'dptime', 'clsspines']
+    aggfile.write(" ".join(fields) + os.linesep)
+
+    for size in final:
+        for spines, record in final[size].items():
+            if record.yescnt > 0:
+                fields = [size, record.dpyestime/record.yescnt, "yes" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+    aggfile.close()
+
+print("Writing Runtime(DFS) plot data...")
+
+with open('runtime_dfs.dat', 'w') as aggfile:
+    fields = ['size', 'dfstime', 'clsspines']
+    aggfile.write(" ".join(fields) + os.linesep)
+
+    for size in final:
+        for spines, record in final[size].items():
+            if record.dfsyes > 0:
+                fields = [size, record.dfsyestime/record.dfsyes, "yes" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+            dfsno = record.yescnt + record.nocnt - record.dfsyes
+            if dfsno > 0:
+                fields = [size, record.dfsnotime/dfsno, "no" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+    aggfile.close()
+
+print("Writing Runtime(BFS) plot data...")
+
+with open('runtime_bfs.dat', 'w') as aggfile:
+    fields = ['size', 'bfstime', 'clsspines']
+    aggfile.write(" ".join(fields) + os.linesep)
+
+    for size in final:
+        for spines, record in final[size].items():
+            if record.bfsyes > 0:
+                fields = [size, record.bfsyestime/record.bfsyes, "yes" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+            bfsno = record.yescnt + record.nocnt - record.bfsyes
+            if bfsno > 0:
+                fields = [size, record.bfsnotime/bfsno, "no" + str(spines)]
+                aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+
+    aggfile.close()
+    
+print("Writing Accuracy plot data...")
+
+with open('accuracy.dat', 'w') as aggfile:
+    fields = ['spines', 'p_solved', 'order', 'yescnt']
+    aggfile.write(" ".join(fields) + os.linesep)
+
+    for spines, record in byspine.items():
+        yescnt = record.yescnt > 0 and record.yescnt or 1
+        fields = [spines, record.dfsyes/yescnt, "dfs", record.yescnt]
+        aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
+        fields = [spines, record.bfsyes/yescnt, "bfs", record.yescnt]
+        aggfile.write(" ".join([str(f) for f in fields]) + os.linesep)
 
     aggfile.close()
 
